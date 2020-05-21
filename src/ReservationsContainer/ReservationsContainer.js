a37dd86dfbe704b6ad0433c3a72f2c5bbd303081
Madison Randle <madisonrandle25@gmail.com>
2020-05-21 10:58:20 -0500

    Iterate through reservations and render Card element for each res

--- src/ReservationsContainer/ReservationsContainer.js
+++ src/ReservationsContainer/ReservationsContainer.js
@@ -3,8 +3,25 @@ import Card from '../Card/Card';
 
 const ReservationsContainer = ({ reservations }) => {
 
+  const displayCard = () => {
+    return reservations.map(reservation => {
+      return (
+        <Card
+          key={reservation.id}
+          id={reservation.id}
+          name={reservation.name}
+          date={reservation.date}
+          time={reservation.time}
+          number={reservation.number}
+        />
+      )
+    })
+  }
+
   return (
-    <p>ReservationsContainer</p>
+    <React.Fragment>
+      { displayCard() }
+    </React.Fragment>
   )
 }
 
