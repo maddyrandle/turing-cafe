2b35c9e6c565f1665010a8f32d4dbdbe6be36a5c
Madison Randle <madisonrandle25@gmail.com>
2020-05-21 11:15:06 -0500

    Create JSX for Form element

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -13,9 +13,43 @@ class Form extends Component {
   }
 
   render() {
-    console.log('form state: ', this.state);
+
     return (
-      <p>Form will go here</p>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.updateFormWithString}
+          required
+        />
+        <input
+          type="text"
+          name="date"
+          placeholder="Date(mm/dd)"
+          value={this.state.date}
+          onChange={this.updateFormWithString}
+          required
+        />
+        <input
+          type="text"
+          name="time"
+          placeholder="Time"
+          value={this.state.time}
+          onChange={this.updateFormWithString}
+          required
+        />
+        <input
+          type="number"
+          name="number"
+          placeholder="Number of guests"
+          value={this.state.number}
+          onChange={this.updateFormWithNumber}
+          required
+        />
+        <button className="makeResBtn">Make Reservation</button>
+      </form>
     )
   }
 }
