2ab7ed575712af41a181b20158529587f8491e7b
Madison Randle <madisonrandle25@gmail.com>
2020-05-21 11:05:18 -0500

    Create JSX of a Card, style

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,7 +4,13 @@ import './Card.css';
 const Card = ({ id, name, date, time, number }) => {
 
   return (
-
+    <article className="card-wrapper">
+      <h1>{name}</h1>
+      <p>{date}</p>
+      <p>{time} pm</p>
+      <p>Number of guests: {number}</p>
+      <button className="cancelBtn">Cancel</button>
+    </article>
   )
 }
 
